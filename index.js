6a22a772307c7d7e6a5bba63c5d2fe6eb6998236
Ben McCann <322311+benmccann@users.noreply.github.com>
2025-09-15 09:55:29 -0700

    switch to tinyglobby

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import fg from 'fast-glob'
+import { glob, isDynamicPattern } from 'tinyglobby'
 import arrayify from 'array-back'
 import { promises as fs } from 'fs'
 import path from 'path'
@@ -47,8 +47,8 @@ class FileSet {
         }
       } catch (err) {
         if (err.code === 'ENOENT') {
-          if (fg.isDynamicPattern(file)) {
-            const found = await fg.glob(file, { onlyFiles: false, markDirectories: true })
+          if (isDynamicPattern(file)) {
+            const found = await glob(file, { onlyFiles: false, markDirectories: true, expandDirectories: false })
             if (found.length) {
               if (options.globResultSortFn) {
                 found.sort(options.globResultSortFn)
